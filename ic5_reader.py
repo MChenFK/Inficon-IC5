a4ffbd7f80c33ffaf8bc73b56ba11c609dc446e7
MChenFK <michaelchen1923@gmail.com>
2025-05-28 15:55:02 -0400

    project setup

---
+++ ic5_reader.py
@@ -0,0 +1,12 @@
+class IC5Reader:
+    def __init__(self, port='/dev/ttyUSB0', baudrate=9600):
+        # Initialize serial or communication interface here
+        pass
+
+    def read_data(self):
+        # Parse IC5 output from serial or file
+        return {}
+
+    def close(self):
+        # Close any open connections
+        pass
---
+++ utils.py
@@ -0,0 +1,3 @@
+def parse_ic5_output(raw_data):
+    # Turn raw data into structured format
+    return {}
